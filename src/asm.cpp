d6efe831b1c8048641de58be4ef0b124a9bc44cd
syoch <hacksyoch@gmail.com>
2021-02-10 18:05:21 +0900

    update: add internal function of split_lines
    That's split string by newline

--- src/asm.cpp
+++ src/asm.cpp
@@ -1,5 +1,22 @@
 #include <asm.h>
 
+void split_lines(std::wstring source, std::vector<std::wstring> &dest)
+{
+    std::wstring buffer;
+    for (auto ch : source)
+    {
+        if (ch == '\n')
+        {
+            dest.emplace_back(buffer);
+            buffer.clear();
+        }
+        else
+        {
+            buffer += ch;
+        }
+    }
+}
+
 std::vector<Assembler::Inst> Assembler::PPC(std::wstring source)
 {
 }
